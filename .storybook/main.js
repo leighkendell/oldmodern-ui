fc65b4caec43a5b9896975eb274730086c15b466
Leigh Kendell <leigh@leighkendell.com>
2022-01-16 17:13:35 +0800

    Changelog for v0.1.0 [skip ci]

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,11 @@ const { vanillaExtractPlugin } = require('@vanilla-extract/vite-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
   framework: '@storybook/react',
   core: {
     builder: 'storybook-builder-vite',
---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# v0.0.1 (Sun Jan 16 2022)
+
+- Created first version of the design system, with `Button` component.
+
+#### Authors: 1
+
+- Leigh Kendell (leigh@leighkendell.com)
